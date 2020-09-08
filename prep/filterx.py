e251b61a6d516a0c46e2b02bccaf7bc8b946687a
Ward Gratten (PAUS) <Gratten.Ward@syntegon.com>
2020-09-08 14:02:38 -0400

    Several new notebooks, data cleansing progress nearly complete

---
+++ prep/filterx.py
@@ -0,0 +1,30 @@
+def sld(df):
+    # remove all non-sld rows, exclude drawings
+    df1 = df[df['Format'] == 'SLDPRT']
+    df2 = df[df['Format'] == 'SLDASM']
+    df = df1.append([df2])
+    df['name_low'] = df['Name'].str.lower()
+    return df
+
+def sld_drw(df):
+    # remove all non-sld rows, include drawings
+    df1 = df[df['Format'] == 'SLDPRT']
+    df2 = df[df['Format'] == 'SLDASM']
+    df3 = df[df['Format'] == 'SLDDRW']
+    df = df1.append([df2, df3])
+    df['name_low'] = df['Name'].str.lower()
+    return df
+
+def paths(target, df):
+    # remove all non-sld rows, exclude drawings
+    df1 = df[df['path1'] == target]
+    df2 = df[df['path2'] == target]
+    df3 = df[df['path3'] == target]
+    df = df1.append([df2, df3])
+    return df
+
+def drw(df):
+    # returns df of SLDDRW only
+    df3 = df[df['Format'] == 'SLDDRW']
+    df3['Name'] = df3['Name'].str.lower()
+    return df3
--- utility/generate.py
+++ utility/generate.py
@@ -21,3 +21,20 @@ def list(fileName, d):
         if len(v) == 2:
             outputFile.write(str(v[0]) + '\\' + str(k) + ';' + str(v[1]) + '\\' + str(k) + '\n')
     outputFile.close()
+
+def batch2(file_name, files_to_move):
+    import os
+    os.chdir('H:/temp/data_cleanse_batch_files')
+    moveFileName = file_name + '.txt'
+    moveFile = open(moveFileName, 'w')
+    for i in files_to_move:
+        print(i)
+        newLoc_path = i
+        newLoc_path = newLoc_path[7:]
+        newLoc_path = newLoc_path[:newLoc_path.rindex('\\')]
+        newLoc_path = newLoc_path.replace(' ', '')
+        newLoc_path = newLoc_path.replace(',', '')
+        newLoc = 'C:\\Users\\wdg1dcr\\Desktop\\dupBackup\\movedFiles2\\' + newLoc_path
+        moveFile.write(f'md {newLoc} 2> nul & move \"{i}\"*.* {newLoc}\n')
+    moveFile.close()
+    os.chdir('C:\\Users\\WDG1DCR\\Desktop\\my_projects\\data_analysis\\data_cleanse')
